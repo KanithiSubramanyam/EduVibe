a0a5c1147f4f2eec031a22e6d4974d34fa41bf10
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-03-20 13:07:16 +0530

    eduVibe project initialized

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <div className='Heading'>
+                    EduVibe
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
